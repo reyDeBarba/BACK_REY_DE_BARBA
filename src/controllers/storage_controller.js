2c80d47618a40066c086e94708a4af325ae2bacf
neriheredia <developer.heredia@gmail.com>
2022-12-07 21:11:56 -0300

    feat: added routes storage

--- src/controllers/storage_controller.js
+++ src/controllers/storage_controller.js
@@ -4,8 +4,38 @@ const storageUploadImage = async (req, res) => {
   try {
     const result = await cloudinary.v2.uploader.upload(req.file.path);
 
-    console.log({ result });
-  } catch (error) {}
+    if (!result)
+      return res.status(500).json('Ocurrio un problema al subir la imagen');
+
+    const data = {
+      fileName: req.body.name,
+      photo: result.secure_url,
+      photoPublicId: result.public_id,
+    };
+
+    res.status(200).json({ status: 200, message: 'Upload image', data });
+  } catch (error) {
+    res.status(500).json({ status: 500, message: 'Ops, upload image error' });
+  }
 };
 
-export { storageUploadImage };
+const storageUpdateImage = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await cloudinary.v2.uploader.destroy(id);
+
+    const result = await cloudinary.v2.uploader.upload(req.file.path);
+    if (!result)
+      return res.status(500).json('Ocurrio un problema al subir la imagen');
+    const data = {
+      fileName: req.body.name,
+      photo: result.secure_url,
+      photoPublicId: result.public_id,
+    };
+    res.status(200).json({ status: 200, message: 'Upload image', data });
+  } catch (error) {
+    res.status(500).json({ status: 500, message: 'Ops, upload image error' });
+  }
+};
+
+export { storageUploadImage, storageUpdateImage };
--- src/routes/storage.js
+++ src/routes/storage.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { storageUploadImage } from '../controllers/storage_controller.js';
+import {
+  storageUpdateImage,
+  storageUploadImage,
+} from '../controllers/storage_controller.js';
 import { upload } from '../middleware/multerUpload.js';
 
 const router = Router();
 
 router.post('/upload', upload.single('image'), storageUploadImage);
+
+router.put('/update/:id', upload.single('image'), storageUpdateImage);
 
 export default router;
